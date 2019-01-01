3f3a59cb9fa42b646021c35e9776edacc24b62bf
Ian Murphy <3jackdaws@gmail.com>
2019-01-01 00:48:42 -0800

    I like where this is right now

--- components.js
+++ components.js
@@ -2,3 +2,83 @@
 Vue.component("page-break", {
     template:"<div class='page-break'></div>"
 });
+
+Vue.component("flexbox", {
+    props:{
+        "space-around":Boolean
+    },
+    template:"<div :class='{grid:true, around:spaceAround}'><slot></slot></div>"
+});
+
+Vue.component("resume-footer", {
+
+    template:"<div class='grid around bottom'><slot></slot></div>"
+});
+
+
+Vue.component("experience", {
+    props:['data'],
+    template:`
+        <div class='section'>
+            <h3 class="header">Experience</h3>
+            <div v-for='job in data' class='job sub-section'>
+                <h5 class="header">{{ job.title }} &middot; {{ job.company }} &middot; {{ job.from }} - {{ job.to }}</h5>
+                
+                <p>{{ job.description }}</p>
+                <ul class="results">
+                    <li v-for="r in job.results"><span>{{ r }}</span></li>
+                </ul>
+                <h6 class="inline-header" v-if="job.technologies">Technologies:</h6><span v-for="tech in job.technologies" class="tech tag">{{tech}}</span>
+            </div>
+        </div>
+    `
+});
+
+Vue.component("education", {
+    props:['data'],
+    template:`
+        <div class='section'>
+            <h3 class="header">Education</h3>
+            <div v-for='degree in data' class='sub-section'>
+                <h5 class="header">{{ degree.type }} in {{ degree.subject }} &middot; {{ degree.date }} &middot; {{ degree.school }}</h5>
+                <p>{{ degree.summary }}</p>
+            </div>
+        </div>
+    `
+});
+
+Vue.component("skills", {
+    props:['data'],
+    template:`
+        <div class='section'>
+            <h3 class="header">Skills</h3>
+            <div v-for='skill in data' class='sub-section'>
+                <h4 class="inline-header">{{ skill.name }}&middot;</h4> 
+                <span>{{ skill.desc }}</span>
+            </div>
+        </div>
+    `
+});
+
+Vue.component("simple-section", {
+    props:['name'],
+    template:`
+        <div class='section'>
+            <h3 class="header">{{name}}</h3>
+            <slot></slot>
+        </div>
+    `
+});
+
+
+Vue.component("column", {
+    props:['width'],
+    template:`<div class='column' :style='{flexBasis:width + "%"}'><slot></slot></div>`
+});
+
+Vue.component("circle-callout", {
+    props:{size:Number, outline:Boolean},
+    template:`
+        <div :class="{circle:true, outline:outline}" :style="{height:size + 'px', width:size + 'px',fontSize: (size/25) + 'em'}"><slot></slot></div>
+    `
+});
---
+++ data.js
@@ -0,0 +1,87 @@
+DATA = {
+    applicant:"Ian Murphy",
+    role:"Software Engineer",
+    website:"isogen.net",
+    linkedin:"linkedin.com/in/ian-m-murphy/",
+    email:"ian@isogen.net",
+    phone:"(503) 830-2275",
+    github:"github.com/3jackdaws",
+    objective:"Testing 123",
+    experience:[
+        {
+            company:"Daimler Trucks NA",
+            title:"Cloud Automation Intern",
+            from:"June 2018",
+            to:"Dec 2018",
+            description:"",
+            results:[
+
+            ],
+            technologies:[
+                "Django",
+                "jQuery",
+                "Vue.js",
+                "Azure",
+                "Docker",
+                "Kubernetes",
+                "vmWare",
+
+            ]
+        },
+        {
+            company:"Tektronix",
+            title:"Software Engineer Intern",
+            from:"June 2017",
+            to:"Dec 2017",
+            description:"Designed and developed self-service developer tools for product licensing, visual API testing, and building evaluation software. Dockerized high-performance video network monitoring appliance software. Created Cloud provider images (AMIs) from marketplace base OS images for network video monitoring products.",
+            results:[
+                "98% reduction in developer time spent obtaining Product licenses",
+                "99% reduction in Product software package build time",
+            ],
+            technologies:[
+                "Python",
+                "JavaScript",
+                "PHP",
+                "PostgreSQL",
+                "Docker",
+                "AWS",
+            ]
+        },
+        {
+            company:"Vobile, Inc",
+            title:"R&D Intern",
+            from:"December 2016",
+            to:"January 2017",
+            description:"Oversaw office-wide Windows 10 workstation upgrades. Performed out of box setup and installation of new workstations. ",
+            technologies:[
+                "Python",
+                "Docker",
+            ]
+        },
+        {
+            company:"Vobile, Inc",
+            title:"R&D Intern",
+            from:"June 2016",
+            to:"September 2016",
+            description:"Accelerated internal documentation software migration by designing format conversion scripts in Python. Dockerized inventory managment software. Performed intra-office tech support.",
+
+
+        },
+    ],
+    education:[
+        {
+            type:"BS",
+            subject:"Computer Engineering Tech",
+            school:"Oregon Insitute of Technology",
+            date:"2018",
+            summary:""
+        }
+    ],
+    skills:[
+        {
+            name:"HTML",
+            desc:"Blah"
+        },
+
+    ]
+};
